ffb8b6c414e348f0eb3e784c3b5a61d087d7926b
Mahmoud Galal <mahmoudgalal57@yahoo.com>
2022-05-14 21:10:31 +0300

    Adding multi-dex support
    - Adding a prebuilt debug apk

--- app/src/main/java/com/mgalal/freenowtask/FreeNowTaskApplication.kt
+++ app/src/main/java/com/mgalal/freenowtask/FreeNowTaskApplication.kt
@@ -1,8 +1,8 @@
 package com.mgalal.freenowtask
 
-import android.app.Application
+import androidx.multidex.MultiDexApplication
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class FreeNowTaskApplication: Application() {
+class FreeNowTaskApplication: MultiDexApplication() {
 }
